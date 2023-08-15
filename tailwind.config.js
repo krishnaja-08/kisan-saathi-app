020683ebaacaa727c82eb7c9b4f3f6446c058359
krishnaja-08 <akulakrishnaja1614@gmail.com>
2023-08-15 20:06:57 +0530

    work

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     extend: {
       backgroundImage: {
         "home-hero": "url('/home-bg.png')",
+        "slide1" : "url('/slide1.png')",
+        "landing" : "url('/landing.png')",
       },
+      backgroundSize: {
+        
+        '100%': '100%',
+        
+      }
     },
   },
   plugins: [],
