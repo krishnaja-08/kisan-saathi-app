39727db3809ec019c718fad4a088bbdf6bbf0ec6
Gopal Kaul <gopalkaul5@gmail.com>
2023-08-18 19:00:06 +0530

    Fix navbar

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
   // on navbar scroll change the bg of navbar to white
-  const [val, setVal] = useState(false)
+  const [val, setVal] = useState(false);
   const navRef = useRef(null);
   useEffect(() => {
     // event listener to see scroll
@@ -15,10 +15,10 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col fixed top-0 w-full left-0 z-50 px-12 py-8">
       <div
         ref={navRef}
-        className="flex flex-row justify-between fixed top-0 w-full left-0 z-50 px-12 py-8 transition-colors duration-300 ease-in-out"
+        className="flex flex-row justify-between  transition-colors duration-300 ease-in-out"
       >
         <img src="logo.png" className="h-[4em] px-8"></img>
         <div className="md:flex hidden flex-row gap-8 text-[#093A01] font-semibold">
@@ -33,8 +33,21 @@ export default function Navbar() {
         <div className="flex flex-row gap-8 ">
           <img src="search.png" className="h-[2em]"></img>
           <div className="flex flex-col">
-          <img onClick={() => { setVal(!val) }} src="menu.png" className="h-[2em]"></img>
-          <div className={`${val === true ? "flex" : "hidden"} md:hidden flex-col gap-8 text-[#093A01] font-semibold`}>
+            <img
+              onClick={() => {
+                setVal(!val);
+              }}
+              src="menu.png"
+              className="h-[2em]"
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${
+          val === true ? "flex" : "hidden"
+        } md:hidden flex-col gap-8 text-[#093A01] font-semibold`}
+      >
         <p>Home</p>
         <p>About us</p>
         <p>Service</p>
@@ -43,11 +56,6 @@ export default function Navbar() {
         <p>Careers</p>
         <p>Contact us</p>
       </div>
-          </div>
-        </div>
-      </div>
-      
     </div>
-
   );
 }
