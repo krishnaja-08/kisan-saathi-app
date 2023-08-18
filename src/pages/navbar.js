2c840679077dd164be30d3e3bd80df3107ae19cf
krishnaja-08 <akulakrishnaja1614@gmail.com>
2023-08-18 18:57:08 +0530

    thehrnet

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
   // on navbar scroll change the bg of navbar to white
+  const [val, setVal] = useState(false)
   const navRef = useRef(null);
   useEffect(() => {
     // event listener to see scroll
@@ -14,13 +15,13 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
   return (
-    <div>
+    <div className="flex flex-col">
       <div
         ref={navRef}
         className="flex flex-row justify-between fixed top-0 w-full left-0 z-50 px-12 py-8 transition-colors duration-300 ease-in-out"
       >
         <img src="logo.png" className="h-[4em] px-8"></img>
-        <div className="flex flex-row gap-8 text-[#093A01] font-semibold">
+        <div className="md:flex hidden flex-row gap-8 text-[#093A01] font-semibold">
           <p>Home</p>
           <p>About us</p>
           <p>Service</p>
@@ -29,11 +30,24 @@ export default function Navbar() {
           <p>Careers</p>
           <p>Contact us</p>
         </div>
-        <div className="flex flex-row gap-8">
+        <div className="flex flex-row gap-8 ">
           <img src="search.png" className="h-[2em]"></img>
-          <img src="menu.png" className="h-[2em]"></img>
+          <div className="flex flex-col">
+          <img onClick={() => { setVal(!val) }} src="menu.png" className="h-[2em]"></img>
+          <div className={`${val === true ? "flex" : "hidden"} md:hidden flex-col gap-8 text-[#093A01] font-semibold`}>
+        <p>Home</p>
+        <p>About us</p>
+        <p>Service</p>
+        <p>People </p>
+        <p>Featured articles</p>
+        <p>Careers</p>
+        <p>Contact us</p>
+      </div>
+          </div>
         </div>
       </div>
+      
     </div>
+
   );
 }
