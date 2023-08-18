bd8b20f26caa985696a674d24646b5bd965813a7
Gopal Kaul <gopalkaul5@gmail.com>
2023-08-18 21:39:43 +0530

    Fix gopal's oops

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,10 @@ export default function Home() {
     <main className={`${poppins.className}`}>
       <div className="bg-landing bg-contain w-full h-full bg-no-repeat z-50">
         <div className="flex w-full md:w-[50%] pt-[20%] md:pt-[36%] md:pb-[20%] md:py-0 md:px-0 py-20 text-xl md:text-3xl px-10 md:pl-40 flex-col">
-          <h1 className="text-transparent bg-clip-text bg-gradient-to-r font-semibold from-[#FFFFFF]/70 to-[#BCF5B2]">Strengthening<br></br>FPO's</h1><br></br>
+          <h1 className="text-transparent bg-clip-text bg-gradient-to-r font-semibold from-[#FFFFFF]/70 to-[#BCF5B2]">
+            Strengthening<br></br>FPO's
+          </h1>
+          <br></br>
           <p className="text-[#FFFFFF]/70">
             Strengthening FPO’s, Strengthening Agriculture through Economics of
             aggregation by Uniting the farms and farmers through FPO’s
@@ -62,8 +65,8 @@ export default function Home() {
         <img src="android.png" className="md:hidden"></img>
       </div>
       <h2 className="text-2xl md:text-4xl flex gap-2 py-10 justify-center font-semibold">
-          Our  <span className="text-[#25CA0A]"> services</span>
-        </h2>
+        Our <span className="text-[#25CA0A]"> services</span>
+      </h2>
       <div className="flex flex-col justify-center text-[#093A01] font-semibold gap-8 px-20">
         <div className="flex items-center md:flex-row flex-wrap gap-4 md:gap-20">
           <p>Input</p>
@@ -139,9 +142,8 @@ export default function Home() {
         </div>
       </div>
       <h2 className="text-2xl md:text-4xl text-center pt-10 font-semibold">
-          Insight Corner{" "}
-          <span className="text-[#25CA0A]">Featured articles</span>
-        </h2>
+        Insight Corner <span className="text-[#25CA0A]">Featured articles</span>
+      </h2>
       <div className="px-10 md:px-40 relative">
         <button className="swiper-prev absolute left-0 md:left-10 top-1/2 bottom-1/2">
           <img src="/leftbutton.png" className="h-10 md:h-24" />
@@ -279,20 +281,20 @@ export default function Home() {
           Partner&apos;s Trust <span className="text-[#25CA0A]">Our pride</span>
         </h2>
         <div className="flex flex-row gap-8 md:block">
-        <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-20">
-          <img src="atal.png" className="w-[16em]"></img>
-          <img src="garuda.jpeg" className="w-[12em]"></img>
-          <img src="giz.png" className="w-[16em]"></img>
-          <img src="go.png" className="w-[12em]"></img>
-          <img src="Cell.jpg" className="w-[12em]"></img>
-        </div>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-16 md:gap-28">
-          <img src="hyd.jpg" className="w-[12em] h-12"></img>
-          <img src="svm.png" className="w-[12em]"></img>
-          <img src="alpha.png" className="w-[12em]"></img>
-          <img src="vision.png" className="w-[12em]"></img>
-          <img src="waycool.jpg" className="w-[8em]"></img>
-        </div>
+          <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-20">
+            <img src="atal.png" className="w-[16em]"></img>
+            <img src="garuda.jpeg" className="w-[12em]"></img>
+            <img src="giz.png" className="w-[16em]"></img>
+            <img src="go.png" className="w-[12em]"></img>
+            <img src="Cell.jpg" className="w-[12em]"></img>
+          </div>
+          <div className="flex flex-col md:flex-row justify-center items-center gap-16 md:gap-28">
+            <img src="hyd.jpg" className="w-[12em] h-12"></img>
+            <img src="svm.png" className="w-[12em]"></img>
+            <img src="alpha.png" className="w-[12em]"></img>
+            <img src="vision.png" className="w-[12em]"></img>
+            <img src="waycool.jpg" className="w-[8em]"></img>
+          </div>
         </div>
       </div>
       <div className="px-40">
@@ -401,27 +403,43 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className=" bg-[#052100E5]" >
-      <div className="flex justify-center md:px-10 px-4 pt-10 pb-8 text-white flex-wrap md:flex-row gap-10 md:gap-48">
-        <div className="flex flex-col gap-8 pb-10">
-          <p className="font-semibold pb-4">kisan saathi</p>
-          <p>Lorem ipsum dolor <br></br>sit amet consectetur</p>
-        </div>
-        <div className="flex flex-col gap-8">
-          <p className="font-semibold">Our Company</p>
-          <p>About us</p>
-          <p>Contact</p>
-          <p>Join our team</p>
-          <p>Privacy policy</p>
-        </div>
-        <div className="flex flex-col gap-8">
-          <div className="font-semibold">Get in touch</div>
-          <div className="flex flex-row gap-12 pb-2">
-            <a href="https://twitter.com/KisanSaathi" target="_blank"><img src="twitter.png" className="w-[1.5em]"></img></a>
-            <a href="https://www.linkedin.com/company/kisansaathi/" target="_blank"><img src="linkedin.png" className="w-[1.5em]"></img></a>
-            <a href="https://instagram.com/kisan.saathi?igshid=NGExMmI2YTkyZg==" target="_blank"><img src="insta.png" className="w-[1.5em]"></img></a>
-            <a href="https://www.facebook.com/KisanSaathi" target="_blank"><img src="fb.png" className="w-[1.5em]"></img></a>
+      <div className=" bg-[#052100E5]">
+        <div className="flex justify-center md:px-10 px-4 pt-10 pb-8 text-white flex-wrap md:flex-row gap-10 md:gap-48">
+          <div className="flex flex-col gap-8 pb-10">
+            <p className="font-semibold pb-4">kisan saathi</p>
+            <p>
+              Lorem ipsum dolor <br></br>sit amet consectetur
+            </p>
           </div>
+          <div className="flex flex-col gap-8">
+            <p className="font-semibold">Our Company</p>
+            <p>About us</p>
+            <p>Contact</p>
+            <p>Join our team</p>
+            <p>Privacy policy</p>
+          </div>
+          <div className="flex flex-col gap-8">
+            <div className="font-semibold">Get in touch</div>
+            <div className="flex flex-row gap-12 pb-2">
+              <a href="https://twitter.com/KisanSaathi" target="_blank">
+                <img src="twitter.png" className="w-[1.5em]"></img>
+              </a>
+              <a
+                href="https://www.linkedin.com/company/kisansaathi/"
+                target="_blank"
+              >
+                <img src="linkedin.png" className="w-[1.5em]"></img>
+              </a>
+              <a
+                href="https://instagram.com/kisan.saathi?igshid=NGExMmI2YTkyZg=="
+                target="_blank"
+              >
+                <img src="insta.png" className="w-[1.5em]"></img>
+              </a>
+              <a href="https://www.facebook.com/KisanSaathi" target="_blank">
+                <img src="fb.png" className="w-[1.5em]"></img>
+              </a>
+            </div>
             <div className="flex flex-row items-center gap-8">
               <img src="time.png" className="w-[1em]"></img>
               <div className="flex flex-col gap-4">
@@ -440,8 +458,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <p className="text-white md:block text-center px-8 py-8">Copyright 2023 | All Rights Reserved</p>
-      </div>
+      <p className="text-white md:block text-center px-8 py-8">
+        Copyright 2023 | All Rights Reserved
+      </p>
     </main>
   );
 }
