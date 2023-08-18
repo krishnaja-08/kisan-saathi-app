196eaa729f0bf5e17ffb04064af74111ff298682
krishnaja-08 <akulakrishnaja1614@gmail.com>
2023-08-18 18:14:52 +0530

    work

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,13 +18,12 @@ export default function Home() {
   return (
     <main className={`${poppins.className}`}>
       <div className="bg-landing bg-contain w-full h-full bg-no-repeat z-50">
-        <div className="flex w-[60%] pt-[40%] pb-[25%] text-3xl pl-40 flex-col">
+        <div className="flex w-[50%] pt-[36%] pb-[20%] text-3xl pl-40 flex-col">
           <h1 className="text-transparent bg-clip-text bg-gradient-to-r font-semibold from-[#FFFFFF]/70 to-[#BCF5B2]">Empowering Indian Agriculture <br></br>for a thriving tomorrow</h1><br></br>
           <p className="text-[#FFFFFF]/70">
-            The establishment of FPO&apos;s, promoting self-sustainability,<br></br>
-            enhanced farm income and effective agricultural practices with<br></br>
-            strengthened mark linkages
-          </p>
+          Strengthening FPO’s,  Strengthening Agriculture through Economics of aggregation by Uniting the farms and  farmers through FPO’s
+
+          </p><br></br>
           <button className="w-fit px-10 py-5 text-xl rounded-2xl bg-gradient-to-r from-[#DDFCA999] to-[#D2EFA31A] text-white font-bold">See more</button>
         </div>
       </div>
@@ -48,46 +47,21 @@ export default function Home() {
           <img className="w-full h-full object-contain" src="smart.png" />
         </div>
       </div>
-      <div className="flex flex-col items-center px-12 text-[#093A01] py-12 gap-8">
+      <div className="flex flex-col items-center text-[#093A01] py-2 gap-8">
         <h2 className="text-4xl font-semibold">
           Addressing Challenges of{" "}
           <span className="text-[#25CA0A]">Agriculture</span>
         </h2>
-        <div className="flex flex-row w-full justify-between text-[#093A01] font-semibold  px-20">
-          <div className="flex flex-col items-center justify-center">
-            <img src="frame.png" className="w-[20em] pb-8"></img>
-            <p>FPO Lacks Awareness & </p>
-            <p>Approach</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img src="frame1.png" className="w-[20em] pb-8"></img>
-            <p>Sustainability of FPO Business</p>
-            <p> Verticals Finance</p>
-          </div>
-        </div>
-        <div className="flex flex-row justify-center text-[#093A01] font-semibold gap-40">
-          <div className="flex flex-col items-center justify-center">
-            <img src="frame2.png" className="w-[20em] pb-8"></img>
-            <p>Networking FPO Management Output</p>
-            <p> Marketing ( Market Linkage, Post</p>
-            <p> Harvest Mngt., Value addition)</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img src="frame3.png" className="w-[20em] pb-8"></img>
-            <p>Input Business </p>
-            <p>( GPA, Input Marketing )</p>
-          </div>
-        </div>
+        <img src="challenges.png" className="h-[60%]"></img>
       </div>
-      <div className="flex flex-col items-center text-[#093A01] font-semibold gap-8 px-10">
-        <h2 className="text-4xl font-semibold">
-          Our <span className="text-[#25CA0A]">services</span>
+      <h2 className="text-4xl flex gap-2  justify-center font-semibold">
+          Our  <span className="text-[#25CA0A]"> services</span>
         </h2>
-        <div className="flex flex-row gap-20">
-          <div className="flex flex-col items-center justify-center">
-            <img src="access.png" className="w-[12em]"></img>
-            <p>Access to kisanSaathiApp</p>
-          </div>
+      <div className="flex flex-col justify-center text-[#093A01] font-semibold gap-8 px-20">
+  
+        <div className="flex items-center flex-row gap-20">
+          <p >Input</p>
+          
           <div className="flex flex-col items-center justify-center">
             <img src="input.png" className="w-[12em]"></img>
             <p>InputConnect</p>
@@ -96,49 +70,55 @@ export default function Home() {
             <img src="access.png" className="w-[12em]"></img>
             <p>Requirement Chart</p>
           </div>
+          
+          
+        </div>
+        <div className="flex items-center flex-row gap-20">
+          <p>Output</p>
           <div className="flex flex-col items-center justify-center">
             <img src="market.png" className="w-[12em]"></img>
             <p>Market Linkage</p>
           </div>
           <div className="flex flex-col items-center justify-center">
             <img src="access.png" className="w-[12em]"></img>
-            <p>Weekly Harvsting Index</p>
+            <p>Weekly Harvsting </p>
+            <p>Index</p>
           </div>
+          
+          <div className="flex flex-col items-center justify-center">
+            <img src="access.png" className="w-[12em]"></img>
+            <p>Sorting &amp; Grading </p><p>Training</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <img src="market.png" className="w-[12em]"></img>
+            <p>Customized </p><p>Marketing </p><p>Strategies</p>
+          </div>        
         </div>
-        <div className="flex flex-row gap-20">
+        <div className="flex items-center flex-row gap-20">
+          <p>Consultancy</p>
+          <div className="flex flex-col items-center ">
+            <img src="access.png" className="w-[12em]"></img>
+            <p>Access to</p><p> kisanSaathiApp</p>
+          </div>
           <div className="flex flex-col items-center justify-center">
             <img src="tech.png" className="w-[12em]"></img>
-            <p>Technology(Extension Services)</p>
+            <p>Technology<br></br>(Extension<br></br> Services)</p>
           </div>
           <div className="flex flex-col items-center justify-center">
             <img src="access.png" className="w-[12em]"></img>
-            <p>Sorting &amp; Grading Training</p>
+            <p>Service Providers</p><p> Connect</p>
           </div>
           <div className="flex flex-col items-center justify-center">
             <img src="access.png" className="w-[12em]"></img>
-            <p>Service Providers Connect</p>
+            <p>FPO Wise</p><p> Commodity Value</p><p> Chain Creation</p>
+          </div>
+          <div className="items-center justify-center flex flex-col">
+            <img src="access.png" className="w-[12em]"></img>
+            <p>Strengthening</p><p>FPOs Business </p><p>Verticles</p>
           </div>
           <div className="flex flex-col items-center justify-center">
             <img src="swot.png" className="w-[12em]"></img>
-            <p>FPO SWOT Analysis</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img src="access.png" className="w-[12em]"></img>
-            <p>Service Providers Connect</p>
-          </div>
-        </div>
-        <div className="flex flex-row gap-20">
-          <div className="items-center justify-center flex flex-col">
-            <img src="access.png" className="w-[12em]"></img>
-            <p>Strengthening FPOs Business Verticles</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img src="market.png" className="w-[12em]"></img>
-            <p>Customized Marketing Strategies</p>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <img src="access.png" className="w-[12em]"></img>
-            <p>FPO Wise Commodity Value Chain Creation</p>
+            <p>FPO SWOT </p><p>Analysis</p>
           </div>
         </div>
       </div>
@@ -153,6 +133,7 @@ export default function Home() {
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={0}
+          slidesPerView={2}
           loop={true}
           autoplay={{
             delay: 1000,
@@ -166,40 +147,71 @@ export default function Home() {
           <SwiperSlide className="p-20">
             <div className="text-white pl-10 justify-center relative aspect-square flex flex-col bg-slide1 px-4 py-4 bg-no-repeat bg-cover rounded-[100px]">
               <div>
-                <p className="font-bold text-3xl">Lorem ipsum dolor sit amet consectetur</p>
+                <p className="font-bold text-3xl">Forbes magazine, 2019</p>
                 <br></br>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
+                <p>The 'Farmer's Companion' Bringing Together Food Buyers And Producers In Urban India</p>
                 <br />
               </div>
+              <a href="https://www.forbes.com/sites/christinero/2019/05/04/the-farmers-companion-bringing-together-food-buyers-and-producers-in-urban-india/?sh=3408748954bf" target="_blank">
               <button className="px-8 py-4 bg-gray-300/70 backdrop-blur-md rounded-xl text-white font-bold w-fit">
                 Read more
-              </button>
+              </button></a>
             </div>
           </SwiperSlide>
           <SwiperSlide className="p-20">
             <div className="text-white pl-10 justify-center relative aspect-square flex flex-col bg-slide1 px-4 py-4 bg-no-repeat bg-cover rounded-[100px]">
               <div>
-                <p className="font-bold text-3xl">Lorem ipsum dolor sit amet consectetur</p>
+                <p className="font-bold text-3xl">Zenger, 2020</p>
                 <br></br>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
+                <p>Technology Helps Indian Farmers Get a Better Deal</p>
                 <br />
               </div>
+              <a href="https://www.zenger.news/2020/11/20/technology-helps-indian-farmers-get-a-better-deal/" target="_blank">
               <button className="px-8 py-4 bg-gray-300/70 backdrop-blur-md rounded-xl text-white font-bold w-fit">
                 Read more
-              </button>
+              </button></a>
             </div>
           </SwiperSlide>
           <SwiperSlide className="p-20">
             <div className="text-white pl-10 justify-center relative aspect-square flex flex-col bg-slide1 px-4 py-4 bg-no-repeat bg-cover rounded-[100px]">
               <div>
-                <p className="font-bold text-3xl">Lorem ipsum dolor sit amet consectetur</p>
+                <p className="font-bold text-3xl">Niti aayog, 74th page, 2021</p>
                 <br></br>
-                <p>Lorem ipsum dolor sit amet consectetur</p>
+                <p>Agriculture and Allied Sectors</p>
                 <br />
               </div>
+              <a href="https://aim.gov.in/pdf/Agriculture-and-Allied-Sectors.pdf" target="_blank">
               <button className="px-8 py-4 bg-gray-300/70 backdrop-blur-md rounded-xl text-white font-bold w-fit">
                 Read more
-              </button>
+              </button></a>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="p-20">
+            <div className="text-white pl-10 justify-center relative aspect-square flex flex-col bg-slide1 px-4 py-4 bg-no-repeat bg-cover rounded-[100px]">
+              <div>
+                <p className="font-bold text-3xl">Social alpha, 2022</p>
+                <br></br>
+                <p>Winners of techtonic innovations</p>
+                <br />
+              </div>
+              <a href="https://www.linkedin.com/posts/social-alpha_winners-of-techtonic-innovations-in-agritech-activity-6988121728442937344-GFvi?utm_source=share&utm_medium=member_desktop" target="_blank">
+              <button className="px-8 py-4 bg-gray-300/70 backdrop-blur-md rounded-xl text-white font-bold w-fit">
+                Read more
+              </button></a>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="p-20">
+            <div className="text-white pl-10 justify-center relative aspect-square flex flex-col bg-slide1 px-4 py-4 bg-no-repeat bg-cover rounded-[100px]">
+              <div>
+                <p className="font-bold text-3xl">2023</p>
+                <br></br>
+                <p>Niti aayog, 27th page</p>
+                <br />
+              </div>
+              <a href="https://www.niti.gov.in/sites/default/files/2023-02/Compendium-of-75-agri-entrerenuer.pdf" target="_blank">
+              <button className="px-8 py-4 bg-gray-300/70 backdrop-blur-md rounded-xl text-white font-bold w-fit">
+                Read more
+              </button></a>
             </div>
           </SwiperSlide>
           
@@ -213,10 +225,18 @@ export default function Home() {
           Partner&apos;s Trust <span className="text-[#25CA0A]">Our pride</span>
         </h2>
         <div className="flex flex-row justify-center items-center gap-20">
-          <img src="logo1.png" className="w-[16em]"></img>
-          <img src="logo2.png" className="w-[12em]"></img>
-          <img src="logo3.png" className="w-[12em]"></img>
-          <img src="logo4.png" className="w-[12em]"></img>
+          <img src="atal.png" className="w-[16em]"></img>
+          <img src="garuda.jpeg" className="w-[12em]"></img>
+          <img src="giz.png" className="w-[16em]"></img>
+          <img src="go.png" className="w-[12em]"></img>
+          <img src="Cell.jpg" className="w-[12em]"></img>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-28">
+          <img src="hyd.jpg" className="w-[12em] h-12"></img>
+          <img src="svm.png" className="w-[12em]"></img>
+          <img src="alpha.png" className="w-[12em]"></img>
+          <img src="vision.png" className="w-[12em]"></img>
+          <img src="waycool.jpg" className="w-[8em]"></img>
         </div>
       </div>
       <div className="px-40">
@@ -243,7 +263,7 @@ export default function Home() {
         </div>
       </div>
       <div className="pl-20 py-5 text-[#093A01]/95">
-        <div className="relative py-96">
+        <div className="relative py-60">
           <h3 className="font-semibold text-2xl">Contact Us</h3>
           <form
             action="#"
@@ -281,7 +301,7 @@ export default function Home() {
               Submit
             </button>
           </form>
-          <div className="w-[55%] absolute top-0 right-0 h-full">
+          <div className="w-[55%] pt-20 absolute top-0 right-0 h-full">
             <img
               src="/contactus.png"
               className="object-contain w-full h-full"
@@ -290,7 +310,7 @@ export default function Home() {
         </div>
       </div>
       <div className=" bg-[#052100E5]" >
-      <div className="flex justify-center px-10 pt-20 pb-8 text-white flex-row gap-48">
+      <div className="flex justify-center px-10 pt-10 pb-8 text-white flex-row gap-48">
         <div className="flex flex-col gap-8 pb-10">
           <p className="font-semibold pb-4">kisan saathi</p>
           <p>Lorem ipsum dolor <br></br>sit amet consectetur</p>
@@ -305,10 +325,10 @@ export default function Home() {
         <div className="flex flex-col gap-8">
           <div className="font-semibold">Get in touch</div>
           <div className="flex flex-row gap-12 pb-2">
-            <img src="twitter.png" className="w-[1.5em]"></img>
-            <img src="linkedin.png" className="w-[1.5em]"></img>
-            <img src="insta.png" className="w-[1.5em]"></img>
-            <img src="fb.png" className="w-[1.5em]"></img>
+            <a href="https://twitter.com/KisanSaathi" target="_blank"><img src="twitter.png" className="w-[1.5em]"></img></a>
+            <a href="https://www.linkedin.com/company/kisansaathi/" target="_blank"><img src="linkedin.png" className="w-[1.5em]"></img></a>
+            <a href="https://instagram.com/kisan.saathi?igshid=NGExMmI2YTkyZg==" target="_blank"><img src="insta.png" className="w-[1.5em]"></img></a>
+            <a href="https://www.facebook.com/KisanSaathi" target="_blank"><img src="fb.png" className="w-[1.5em]"></img></a>
           </div>
           <div className="flex flex-row items-center gap-8">
             <img src="time.png" className="w-[1em]"></img>
